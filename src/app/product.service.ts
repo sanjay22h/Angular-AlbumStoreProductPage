a0ad78da9ed8981dad5575a3094331a9f2df2fc1
sanjayh <sanjay22h@gmail.com>
2022-05-18 13:43:45 +0530

    Tests 2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Album } from './album';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 
